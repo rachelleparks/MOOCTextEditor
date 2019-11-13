6c34fe62dddb12fc0b7870f21d35e2fe4704a0e7
RaChelle <RaChelle@10.0.69.33>
2019-11-12 18:45:25 -0600

    DictionaryLL class

--- MOOCTextEditor/src/spelling/DictionaryLL.java
+++ MOOCTextEditor/src/spelling/DictionaryLL.java
@@ -12,8 +12,9 @@ public class DictionaryLL implements Dictionary
 	private LinkedList<String> dict;
 	
     // TODO: Add a constructor
-
-
+	public DictionaryLL() {
+		dict = new LinkedList<String>();
+	}
     /** Add this word to the dictionary.  Convert it to lowercase first
      * for the assignment requirements.
      * @param word The word to add
@@ -21,7 +22,13 @@ public class DictionaryLL implements Dictionary
      * (it wasn't already there). */
     public boolean addWord(String word) {
     	// TODO: Implement this method
-        return false;
+    	String wordToAdd = word.toLowerCase();
+    	if (dict.contains(wordToAdd)) {
+    		return false;
+    	} else {
+    		dict.add(wordToAdd);
+            return true;
+    	}
     }
 
 
@@ -29,13 +36,18 @@ public class DictionaryLL implements Dictionary
     public int size()
     {
         // TODO: Implement this method
-        return 0;
+    	 return dict.size();
     }
 
     /** Is this a word according to this dictionary? */
     public boolean isWord(String s) {
         //TODO: Implement this method
-        return false;
+    	String sToCheck = s.toLowerCase();
+    	if (dict.contains(sToCheck)) {
+    		return true;
+    	} else {
+    		return false;
+    	}
     }
 
     
